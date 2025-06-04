ef9ebf2f9eb90c6d48b78c4f7b5db9c9a1783525
Quentin Gevers <quentin.gevers@gmail.com>
2025-06-04 14:15:09 +0000

    Added avg nb of events

--- models/intermediate/olympics_time_to_medal.sql
+++ models/intermediate/olympics_time_to_medal.sql
@@ -41,6 +41,7 @@ avg_time AS(
         sport_cleaned
         , ROUND(AVG(time_to_medal),0) as avg_year_to_medals
         , ROUND(AVG(time_to_gold),0) as avg_year_to_gold
+        , ROUND(AVG(nb_events),0) as avg_nb_events
     FROM time_medals
     GROUP BY sport_cleaned
 ), 
@@ -56,6 +57,7 @@ featured_sports AS(
 SELECT
     sport_cleaned
     , nb_events
+    , avg_nb_events
     , avg_year_to_medals
     , avg_year_to_gold
 FROM avg_time
