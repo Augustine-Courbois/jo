3360aa5866d8256d804cf77683fd55534f837687
Augustine Courbois <augustine.courbois@edu.escp.eu>
2025-06-05 22:11:17 +0000

    ôté t.nb_sport_olympics, t.avg_year_to_medals, t.avg_year_to_gold de la base olympics_genre --> inutile

--- models/intermediate/olympics_sport.sql
+++ models/intermediate/olympics_sport.sql
@@ -30,15 +30,16 @@ AVG(o.gender_inequality_index) as gender_inequality_index,
 AVG(o.prevalence_obesity_adults) as prevalence_obesity_adults,
 AVG(o.is_host) as is_host,
 AVG(o.nb_participations) as nb_participations,
-AVG(o.nb_sport_olympics) as nb_sport_olympics,
-AVG(o.avg_year_to_medals) as avg_year_to_medals,
-AVG(o.avg_year_to_gold) as avg_year_to_gold,
+AVG(t.nb_sport_olympics) as nb_sport_olympics,
+AVG(t.avg_year_to_medals) as avg_year_to_medals,
+AVG(t.avg_year_to_gold) as avg_year_to_gold,
 AVG(m.nb_participation_sport_country) as nb_participation_sport_country,
 AVG(m.first_participation) as first_participation,
 AVG(m.first_medal) as first_medal,
 AVG(m.time_to_medal) as time_to_medal,
 AVG(m.first_gold_medal) as first_gold_medal,
 AVG(m.time_to_gold) as time_to_gold
+
 FROM {{ ref('olympics_genre') }} as o
 
 LEFT JOIN {{ ref('olympics_sport_nb_athlete') }} as a
@@ -46,6 +47,9 @@ ON o.code_year_sport_cleaned = a.code_year_sport_cleaned
 
 LEFT JOIN {{ ref('olympics_time_medals_country') }} as m
 ON o.code_sport_cleaned = m.code_sport_cleaned
+
+LEFT JOIN {{ ref('olympics_time_to_medal') }} as t
+ON o.sport_cleaned = t.sport_cleaned
 
 GROUP BY 
 o.country, 
